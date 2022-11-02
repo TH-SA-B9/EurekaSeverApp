1db32a147a1fd1551697cfb205acb7bdaa5b3a1b
Mạnh Phạm <manhk979@gmail.com>
2022-11-02 21:41:02 +0700

    first commit

---
+++ src/main/java/com/example/eurekaseverapp/EurekaSeverAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaseverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSeverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSeverAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eurekaseverapp/EurekaSeverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaseverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSeverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
